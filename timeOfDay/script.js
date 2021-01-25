1eb2f3ff5287c67c358b27a76961ddf329a27f31
MateusAntonioDev <mateusamalmeida@fac.pe.senac.br>
2021-01-25 14:20:53 -0300

    Update on 'Time Of Day'

--- timeOfDay/script.js
+++ timeOfDay/script.js
@@ -5,11 +5,14 @@ function Time(){
     const hour = now.toLocaleTimeString();
     text.innerHTML = `<p>Now it's ${hour}</p> `;
 
-    if (4 < now.getHours() && now.getHours() < 13){
+    if (4 < now.getHours() && now.getHours() < 12){
         image.innerHTML = `<img src="img/manha.png" alt="Foto de dia">`;
         document.body.style.backgroundColor = '#e2cd9f';
-    } else if (13 <= now.getHours() && now.getHours() < 18) {
-        image.innerHTML = `<img src="img/tarde.png" alt="Foto de tarde">`;
+    } else if (12 <= now.getHours() && now.getHours() < 16) {
+        image.innerHTML = `<img src="img/tarde.png" alt="Foto de tarde">`
+        document.body.style.backgroundColor = '#669BDD'
+    } else if (16 <= now.getHours() && now.getHours() < 18) {
+        image.innerHTML = `<img src="img/fimTarde.png" alt="Foto de tarde">`;
         document.body.style.backgroundColor = '#b9846f';
     } else {
         image.innerHTML = `<img src="img/noite.png" alt="Foto de noite">`;

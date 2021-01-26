99a9b5e79f47c9d65790a43d964938ce40dd11b2
MateusAntonioDev <mateusamalmeida@fac.pe.senac.br>
2021-01-26 11:43:01 -0300

    Added validations in the fifth exercise

--- arrayAnalyzer/script.js
+++ arrayAnalyzer/script.js
@@ -1,23 +1,53 @@
 let numbers = []
-function add() {
-    let number = Number(document.querySelector('input#number').value);
-    numbers.push(number);
-    let list = document.querySelector('ul#list');
+let list = document.querySelector('ul#list');
+const output = document.querySelector('div#output');
 
-    list.innerHTML += `<li>${number}</li>`;
-    
+function add() {
+    let number = document.querySelector('input#number');
+    if (isNumber(number.value) && !inList(number.value, numbers)) {
+        numbers.push(Number(number.value));
+        list.innerHTML += `<li>${number.value}</li>`;
+        output.innerHTML = '';
+    } else {
+        window.alert("Invalid value or already found in the list.");
+    }
+    number.value = '';
+    number.focus();
 }
 
+
 function analyze() {
-    const output = document.querySelector('div#output');
-    output.innerHTML += `<p>There are ${numbers.length} numbers registered</p>`;
-    numbers.sort((a, b) => a - b);
-    output.innerHTML += `<p>The highest recorded value was ${numbers[numbers.length -1]}</p>`;
-    output.innerHTML += `<p>The lowest recorded value was ${numbers[0]}</p>`;
-    let total = 0;
-    for (count in numbers) {
-        total += numbers[count];
+    if (numbers.length == 0) {
+        window.alert("Add values before analyzing.")
+    } else {
+        output.innerHTML = '';
+        output.innerHTML += `<p>There are ${numbers.length} numbers registered</p>`;
+        numbers.sort((a, b) => a - b);
+        output.innerHTML += `<p>The highest recorded value was ${numbers[numbers.length -1]}</p>`;
+        output.innerHTML += `<p>The lowest recorded value was ${numbers[0]}</p>`;
+        let total = 0;
+        for (count in numbers) {
+            total += numbers[count];
+        }
+        output.innerHTML += `<p>The sum off all values is ${total}</p>`;
+        output.innerHTML += `<p>The avarage of the values is ${(total/numbers.length).toFixed(2)}</p>`;
     }
-    output.innerHTML += `<p>The sum off all values is ${total}</p>`;
-    output.innerHTML += `<p>The avarage of the values is ${total/numbers.length}</p>`;
+}
+
+
+
+function isNumber(number) {
+    if (Number(number) >= 1 && Number(number) <= 100) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function inList(number, list) {
+    if (list.indexOf(Number(number)) != -1) {
+        return true;
+    } else {
+        return false;
+    }
 }
